ea593b782d5935b1aa4979ee9e47713a6b3714c1
stanislav.maslow@gmail.com <stanislav.maslow@gmail.com>
2017-10-29 22:51:28 +0200

    implementing basics of react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-//includes
-import './style.scss';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route
+} from "react-router-dom";
 
-class App extends Component {
-    render() {
-        return (
-            <h1>I can see you... </ h1>
-        );
-    }
-}
+import Homepage from "./components/homepage";
+import About from "./components/about";
+import Header from "./components/header";
+import Blog from "./components/blog";
+// import "./style.scss";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+        </ul>
+        <hr/>
+        <Route exac path='/' component={Homepage}/>
+        <Route path='/about' component={About}/>
+        <Route path='/blog' component={Blog}/>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
