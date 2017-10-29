7cfe6e0c53823f7c62ce972caf8ef1972fa0a74e
stanislav.maslow@gmail.com <stanislav.maslow@gmail.com>
2017-10-29 14:24:55 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+//includes
+import './style.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Hellow </ h1>
+        );
+    }
+}
+
+export default App;
